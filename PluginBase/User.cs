1c4845ff73c39ebfdf0220186d0a5d69d0027425
Samadou OKPAGNIDE <abdoul-samadou.okpagnide1@uqac.ca>
2020-04-13 20:38:26 +0100

    Class PluginBase.User.cs

---
+++ PluginBase/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PluginBase
+{
+    public interface User
+    {
+        string id { get; }
+        string first_name { get; }
+        string last_name { get; }
+        string email { get; }
+
+        void AddUser();
+    }
+}
